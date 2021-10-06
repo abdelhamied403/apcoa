90704b87b84d62c974551f747b27c0c701adf403
Abdelhamied <Abdelhamied403@gmail.com>
2021-10-06 23:07:55 +0200

    oks

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,30 @@
+const api = "testapi";
+
 axios
-  .get("https://jsonplaceholder.typicode.com/users")
+  .get(`${api}/tree`)
   .then((res) => {
-    console.log(res.data);
+    $("#tree p").text(res.data.environmental);
+  })
+  .catch((err) => console.error(err));
+
+axios
+  .get(`${api}/car`)
+  .then((res) => {
+    $("#car p").text(res.data.data);
+  })
+  .catch((err) => console.error(err));
+
+axios
+  .get(`${api}/carTall`)
+  .then((res) => {
+    $("#carTall p month1").text(Object.values(res.data.data)[0]);
+    $("#carTall p month2").text(Object.values(res.data.data)[1]);
+  })
+  .catch((err) => console.error(err));
+
+axios
+  .get(`${api}/cloud`)
+  .then((res) => {
+    $("#cloud p").text(res.data.data);
   })
   .catch((err) => console.error(err));
