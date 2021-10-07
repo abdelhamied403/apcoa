109305e783283ff847c4c33fb9a525ae11f8c0bd
Abdelhamied <Abdelhamied403@gmail.com>
2021-10-07 17:15:52 +0200

    cloud

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,6 +26,6 @@ axios
 axios
   .get(`${api}/prevcarbon.php`)
   .then((res) => {
-    $("#cloud p").text(res.data.data);
+    $("#cloud p").text(res.data.data + " kg");
   })
   .catch((err) => console.error(err));
