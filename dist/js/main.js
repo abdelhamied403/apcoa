e929d24f72e021fdf7717fd501e8dcafc137731d
Abdelhamied <Abdelhamied403@gmail.com>
2021-10-11 22:19:14 +0200

    fixed all static dates

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,19 @@
+const today = new Date();
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 const api = "http://dev.norandum.se";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
@@ -27,6 +43,8 @@ axios
   .then((res) => {
     const val1 = Object.values(res.data.data)[0];
     const val2 = Object.values(res.data.data)[1];
+    const key1 = Object.keys(res.data.data)[0];
+    const key2 = Object.keys(res.data.data)[1];
 
     if (val1 === val2) {
       $(".cars img#img1").attr("src", "assets/carTall.svg");
@@ -39,7 +57,18 @@ axios
       $(".cars img#img2").attr("src", "assets/carShort.svg");
     }
 
-    $(".cars p#val1").text(Object.values(res.data.data)[0] + " KG");
-    $(".cars p#val2").text(Object.values(res.data.data)[1] + " KG");
+    const month1 = +key1.split("-")[1] - 1;
+    const month2 = +key2.split("-")[1] - 1;
+
+    $(".cars p#month1").text(months[month1]);
+    $(".cars p#month2").text(months[month2]);
+    $(".cars p#val1").text(val1 + " KG");
+    $(".cars p#val2").text(val2 + " KG");
   })
   .catch((err) => console.error(err));
+
+const day = today.getDate();
+const month = months[today.getMonth()];
+const year = today.getFullYear();
+
+$(".car #date").text(`${day} ${month} ${year}`);
